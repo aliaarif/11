eccec96067e91a5ae233f5a2b7338ee6862247ae
Mohammed Aarif <ali.aliaaif@gmail.com>
2021-05-30 19:53:43 +0530

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() => runApp(FirstApp());
 
 class FirstApp extends StatelessWidget {
-  //const FirstApp({Key key}) : supur(key:key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
