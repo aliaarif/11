10a6fc2e3897d91f1fa0fd89da00b1640fbbb3cc
Mohammed Aarif <ali.aliaaif@gmail.com>
2021-05-30 21:27:05 +0530

    day1,2,3

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'pages/home_page.dart';
+import 'pages/login_page.dart';
 
 void main() => runApp(FirstApp());
 
 class FirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+      },
+    );
   }
 }
