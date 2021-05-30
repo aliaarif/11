0f389b7ba6eaa53147bd8f232622b26413d65923
Mohammed Aarif <ali.aliaaif@gmail.com>
2021-05-30 19:08:24 +0530

    1

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(FirstApp());
+
+class FirstApp extends StatelessWidget {
+  //const FirstApp({Key key}) : supur(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Container(
+          child: Text("Welcome to 30 days of flutter"),
+        ),
+      ),
+    );
+  }
+}
